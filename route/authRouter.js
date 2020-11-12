b1f4eda7fa6b0a61f220b4df068e2925e1574562
untillness <pvauntil@gmail.com>
2020-11-12 15:57:10 +0700

    add: signin

--- route/authRouter.js
+++ route/authRouter.js
@@ -5,37 +5,69 @@ const bcrypt = require('bcrypt')
 const authRouter = require('express').Router()
 const jwt = require('jsonwebtoken')
 
-authRouter.post('/login', async(req, res) => {
+authRouter.post('/login', async (req, res) => {
     const data = req.body
-    utils.validateReq(res, data)
+    utils.validateReq(res, data, validation.login)
 
-    try{
+    try {
         let getUser = await UserModel.findOne({
             email: data.email
         })
 
-        if(!getUser){
-            throw new Error('email dan password anda salah, silahkan coba lagi') 
+        if (!getUser) {
+            throw new Error('email dan password anda salah, silahkan coba lagi')
         }
 
-        let checkPass = bcrypt.compare(data.password, getUser.password)
-        if(!checkPass){
-            throw new Error('email dan password anda salah, silahkan coba lagi') 
+        let checkPass = await bcrypt.compare(data.password, getUser.password)
+        if (!checkPass) {
+            throw new Error('email dan password anda salah, silahkan coba lagi')
         }
 
-        const token = jwt.sign(getUser, process.env.JWT_SECRET)
+        const token = jwt.sign({
+            name: getUser.name,
+            email: getUser.email,
+            id: getUser._id
+        }, process.env.JWT_SECRET)
 
         return res.send(utils.response(1, 'berhasil masuk', {
             user: getUser,
             token
         }))
-    }catch(e){
+    } catch (e) {
         return res.send(utils.responseError(e.message))
     }
 })
 
-authRouter.post('/signin', ()=>{
-    
+authRouter.post('/signin', async (req, res) => {
+    const data = req.body
+    utils.validateReq(res, data, validation.signin)
+
+    try {
+        let getUser = await UserModel.findOne({
+            email: data.email
+        })
+
+        if (getUser) {
+            throw new Error('email sudah terdaftar, silahkan coba lagi')
+        }
+
+        let saltBcrypt = await bcrypt.genSalt(10)
+        let hashPass = await bcrypt.hash(data.password, saltBcrypt)
+
+        const dataUser = UserModel({
+            name: data.name,
+            email: data.email,
+            password: hashPass
+        })
+
+        let saveUser = await dataUser.save()
+        return res.send(utils.response(1, 'berhasil mendaftar, silahkan masuk untuk melanjutkan', {
+            user: saveUser
+        }))
+
+    } catch (error) {
+        return res.send(utils.responseError(error.message))
+    }
 })
 
 module.exports = authRouter
--- validation/validation.js
+++ validation/validation.js
@@ -1,9 +1,16 @@
 const Joi = require('@hapi/joi')
 const validation = {
-    login: (data)=>{
+    login: (data) => {
         return Joi.object({
             email: Joi.string().required().email(),
             password: Joi.string().required()
+        }).validate(data)
+    },
+    signin: (data) => {
+        return Joi.object({
+            name: Joi.string().required().min(3),
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(6)
         }).validate(data)
     }
 }
